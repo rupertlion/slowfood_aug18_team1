5b7936c3c4f9c5b49a681c29bf3a81aef19b8385
danmalmx <danmalmx@gmail.com>
2018-09-27 17:41:47 +0200

    Renamed feature test files and reorganized feature steps

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -3,7 +3,3 @@ Given("the following registered users exist") do |table|
         create(:user, user)
     end
 end
-  
-Then("I am on the login page") do
-    expect(page.current_path).to eq new_user_session_path
-end
--- features/step_definitions/registration_steps.rb
+++
@@ -1,3 +0,0 @@
-Then("I am on the signup page") do
-    expect(page.current_path).to eq new_user_registration_path
-end
