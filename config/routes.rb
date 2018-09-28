ba1f06c971b64f17f0ee7398036a70b0a6ad433d
Albert Stjärne <albert.stjarne@gmail.com>
2018-09-28 11:37:32 +0200

    installing cartify

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :products, only: [:show]
+  mount Cartify::Engine, at: '/'
   devise_for :users
   root controller: :products, action: :index
   resources :products
