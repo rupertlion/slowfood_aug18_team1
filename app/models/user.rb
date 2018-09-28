7cbea33c017aad134fb061b4133c18a9397005b4
Albert Stjärne <albert.stjarne@gmail.com>
2018-09-28 11:10:48 +0200

    updating application controller and migrating db

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :orders, class_name: 'Cartify::Order', foreign_key: :user_id
+  has_one :billing, class_name: 'Cartify::Billing', foreign_key: :user_id
+  has_one :shipping, class_name: 'Cartify::Shipping', foreign_key: :user_id
+  has_many :addresses, class_name: 'Cartify::Address', foreign_key: :user_id       
 end
