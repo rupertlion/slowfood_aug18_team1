3c8d7c2ff214ee219de053b6c38c32dd9c195b37
danmalmx <danmalmx@gmail.com>
2018-09-30 12:46:19 +0200

    reduced number of seed for visibility purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,27 @@
 require 'faker'
 categories = Category.create([{ name: 'Starters'}, {name: 'Mains'}, {name: 'Desserts'}, {name: 'Sides'}, {name: 'Beverages'}])
 
-5.times do
-    Product.create!(name: Faker::Food.dish,
-                 description:Faker::Food.description, price: rand(20..900), category: categories.first )
-  end
+3.times do
+  Product.create!(name: Faker::Food.dish,
+  description:Faker::Food.description, price: rand(20..900), category: categories.first )
+end
 
-  5.times do
-    Product.create!(name: Faker::Food.dish,
-                 description:Faker::Food.description, price: rand(20..900), category: categories.second )
-  end
+3.times do
+  Product.create!(name: Faker::Food.dish,
+  description:Faker::Food.description, price: rand(20..900), category: categories.second )
+end
 
-  5.times do
-    Product.create!(name: Faker::Food.dish,
-                 description:Faker::Food.description, price: rand(20..900), category: categories.fourth )
-  end
+3.times do
+  Product.create!(name: Faker::Food.dish,
+  description:Faker::Food.description, price: rand(20..900), category: categories.fourth )
+end
 
-  5.times do
-    Product.create!(name: Faker::Dessert.variety,
-                 description:Faker::Dessert.flavor, price: rand(20..900), category: categories.third )
-  end
+3.times do
+  Product.create!(name: Faker::Dessert.variety,
+  description:Faker::Dessert.flavor, price: rand(20..900), category: categories.third )
+end
 
-  5.times do
-    Product.create!(name: Faker::Beer.name,
-                 description:Faker::Beer.style, price: rand(20..900), category: categories.fifth )
-  end
+3.times do
+  Product.create!(name: Faker::Beer.name,
+  description:Faker::Beer.style, price: rand(20..900), category: categories.fifth )
+end
